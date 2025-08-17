79bb3554243164ab970d7ad6f491cba5f9f3a622
MuhammadJghz7 <145560831+MuhammadJghz7@users.noreply.github.com>
2025-08-17 17:29:09 +0330

    Create default-courses.js

---
+++ 05/26/default-courses.js
@@ -0,0 +1,7 @@
+const defaultCourses = [
+  { name: "ریاضی عمومی ۱", code: 101, practical: 0, theory: 3, type: "پایه" },
+  { name: "فیزیک عمومی ۱", code: 102, practical: 1, theory: 3, type: "پایه" },
+  { name: "زبان عمومی", code: 201, practical: 0, theory: 2, type: "عمومی" },
+  { name: "مبانی برنامه‌نویسی", code: 301, practical: 1, theory: 2, type: "اصلی" },
+  { name: "آز فیزیک ۱", code: 401, practical: 1, theory: 0, type: "اختیاری" }
+];
