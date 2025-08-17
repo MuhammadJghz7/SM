cb40e1e1419cb234059ab3a8da2315b06dda60b8
MuhammadJghz7 <145560831+MuhammadJghz7@users.noreply.github.com>
2025-08-17 17:47:19 +0330

    Update default-courses.js

--- 05/26/default-courses.js
+++ 05/26/default-courses.js
@@ -3,6 +3,6 @@ const defaultCourses = [
   { name: "فیزیک عمومی ۱", code: 102, practical: 1, theory: 3, type: "پایه" },
   { name: "زبان عمومی", code: 201, practical: 0, theory: 2, type: "عمومی" },
   { name: "مبانی برنامه‌نویسی", code: 301, practical: 1, theory: 2, type: "اصلی" },
-  { name: "آز فیزیک ۱", code: 401, practical: 1, theory: 0, type: "اختیاری" }
+  { name: "آز فیزیک ۱", code: 401, practical: 1, theory: 0, type: "اختیاری" },
   { name: “مکانیک تحیلی ۱”, code: 501, practical: 0, theory: 3, type: “اصلی” }
 ];
